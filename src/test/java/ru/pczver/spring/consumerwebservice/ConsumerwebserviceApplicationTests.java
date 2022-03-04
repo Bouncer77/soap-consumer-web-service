a52c646f89746d43f2c4559c5ec8b8a775045790
Bouncer77 <bouncer77rus@gmail.com>
2022-03-04 10:21:35 +0300

    init

---
+++ src/test/java/ru/pczver/spring/consumerwebservice/ConsumerwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pczver.spring.consumerwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
